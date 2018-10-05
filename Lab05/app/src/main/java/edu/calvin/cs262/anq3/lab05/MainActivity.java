4174a94e378a76c585c28b80715b30b78e257e69
andrewmanq <Everest42>
2018-10-05 17:06:50 -0400

    hi

--- Lab05/app/src/main/java/edu/calvin/cs262/anq3/lab05/MainActivity.java
+++ Lab05/app/src/main/java/edu/calvin/cs262/anq3/lab05/MainActivity.java
@@ -1,12 +1,24 @@
 package edu.calvin.cs262.anq3.lab05;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<String>{
 
     TextView mTitleText;
     TextView mAuthorText;
@@ -19,10 +31,94 @@ public class MainActivity extends AppCompatActivity {
         mTitleText = findViewById(R.id.titleText);
         mAuthorText = findViewById(R.id.authorText);
         mBookInput = findViewById(R.id.bookInput);
+
+        if(getSupportLoaderManager().getLoader(0)!=null){
+            getSupportLoaderManager().initLoader(0,null,this);
+        }
+
     }
 
     public void searchBooks(View view) {
+
         String queryString = mBookInput.getText().toString();
-        new FetchBook(mTitleText, mAuthorText).execute(queryString);
+
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected() && queryString.length()!=0) {
+//            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+
+            Bundle queryBundle = new Bundle();
+            queryBundle.putString("queryString", queryString);
+            getSupportLoaderManager().restartLoader(0, queryBundle,this);
+
+            mAuthorText.setText("");
+            mTitleText.setText("Loading...");
+        }
+
+        else {
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText("Please enter a search term");
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText("Please check your network connection and try again.");
+            }
+        }
+    }
+
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int i, @Nullable Bundle bundle) {
+        return new BookLoader(this, bundle.getString("queryString"));
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String s) {
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+            for(int i = 0; i<itemsArray.length(); i++){
+                JSONObject book = itemsArray.getJSONObject(i);
+                String title=null;
+                String authors=null;
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+
+
+                if (title != null && authors != null){
+                    mTitleText.setText(title);
+                    mAuthorText.setText(authors);
+                    return;
+                }
+            }
+
+            mTitleText.setText("No Results Found");
+            mAuthorText.setText("");
+
+
+        } catch (Exception e){
+            mTitleText.setText("No Results Found");
+            mAuthorText.setText("");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
     }
 }
